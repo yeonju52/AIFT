27e727cb09609daf3b886d75f8f5502134a7f422
JiwonHeo <90255456+JiwonHeo01@users.noreply.github.com>
2022-12-09 15:32:16 +0900

    start->데이터 변환

--- Research/real-time_TR/pytrader.py
+++ Research/real-time_TR/pytrader.py
@@ -9,7 +9,7 @@ accounts = kiwoom.GetLoginInfo("ACCNO")
 stock_account = accounts[0]
 
 # 삼성전자, 10주, 시장가주문 매수
-kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 10, 0, "03", "")
+# kiwoom.SendOrder("시장가매수", "0101", stock_account, 1, "005930", 10, 0, "03", "")
 
 # 삼성전자, 10주, 시장가주문 매도
 kiwoom.SendOrder("시장가매도", "0101", stock_account, 2, "005930", 10, 0, "03", "")
--- Research/real-time_TR/start.py
+++ Research/real-time_TR/start.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import *
 import datetime
+from const import *
+
 
 
 class MyWindow(QMainWindow):
@@ -9,6 +11,8 @@ class MyWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Real")
         self.setGeometry(300, 300, 300, 400)
+
+        self.universe_realtime_transaction_info = {}
 
         btn = QPushButton("Register", self)
         btn.move(20, 20)
@@ -20,12 +24,12 @@ class MyWindow(QMainWindow):
 
         self.ocx = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.ocx.OnEventConnect.connect(self._handler_login)
-        self.ocx.OnReceiveRealData.connect(self._handler_real_data)
+        self.ocx.OnReceiveRealData.connect(self._on_receive_real_data)
         self.CommmConnect()
 
     def btn_clicked(self):
-        self.SetRealReg("1000", "005930", "20;10", 0)
-        #self.SetRealReg("2000", "", "215;20;214", 0)
+        #self.SetRealReg("1000", "005930", "20", 0)
+        self.SetRealReg("2000", "", "215;20;214", 0)
         print("called\n")
 
     def btn2_clicked(self):
@@ -59,10 +63,56 @@ class MyWindow(QMainWindow):
         data = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, fid) 
         return data
 
+    def _on_receive_real_data(self, code, real_type, data):
+        if real_type == "장시작시간":
+            gubun =  self.GetCommRealData(code, 215)
+            remained_time =  self.GetCommRealData(code, 214)
+            print(gubun, remained_time)
+            if gubun == 4:
+                self.DisConnectRealData("1000")
+
+
+        elif real_type == "주식체결":
+            signed_at = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 20)
+
+            close = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 10)
+            close = abs(int(close))
+
+            high = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 17)
+            high = abs(int(high))
+
+            open = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 16)
+            open = abs(int(open))
+
+            low = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 18)
+            low = abs(int(low))
+
+            accum_volume = self.ocx.dynamicCall("GetCommRealData(QString, int)", code, 13)
+            accum_volume = abs(int(accum_volume))
+
+            print(signed_at, close, high, open, low, accum_volume)
+
+            if code not in self.universe_realtime_transaction_info:
+                self.universe_realtime_transaction_info.update({code: {}})
+
+            self.universe_realtime_transaction_info[code].update({
+                "체결시간": signed_at,
+                "시가": open,
+                "고가": high,
+                "저가": low,
+                "현재가": close,
+                "누적거래량": accum_volume
+            })
+
+            def get_fid(search_value):
+                keys = [key for key, value in FID_CODES.items() if value == search_value]
+                return keys[0]
+
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyWindow()
     window.show()
+    
     app.exec_()
