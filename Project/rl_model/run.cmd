a2622e11efe11a761b2670d508e661e2518c06f4
yeonjuLee <yeonju509@gmail.com>
2022-12-10 13:08:11 +0900

    temp: 변경전으로 돌아가야함
    temp

--- Project/rl_model/run.cmd
+++ Project/rl_model/run.cmd
@@ -3,34 +3,36 @@ setlocal enabledelayedexpansion
 
 call C:\Users\USER\Anaconda3\Scripts\activate.bat
 
-@REM 삼성전자: 005930
-@REM 현대차: 005380
-@REM 카카오: 035720
+@REM kodex_200: 069500
+@REM kodex_inverse: 114800
+@REM kodex_kospi: 226490
 
-python main.py --mode train --ver v3 --name 005930 --stock_code 005930 --rl_method a2c --net lstm --start_date 20180101 --end_date 20191231
-python main.py --mode train --ver v3 --name 005380 --stock_code 005380 --rl_method a2c --net lstm --start_date 20180101 --end_date 20191231
-python main.py --mode train --ver v3 --name 035720 --stock_code 035720 --rl_method a2c --net lstm --start_date 20180101 --end_date 20191231
+for %%c in (069500 114800 226490) do (
+    for %%n in (dnn lstm cnn) do (
+        @REM train
+        python main.py --mode train --ver v1 --name %%c --stock_code %%c --rl_method dqn --net %%n --start_date 20211206090000 --end_date 20220806000000
 
-python main.py --mode test --ver v3 --name 005930 --stock_code 005930 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode test --ver v3 --name 005380 --stock_code 005380 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode test --ver v3 --name 035720 --stock_code 035720 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
+        @REM test
+        python main.py --mode test --ver v1 --name %%c --stock_code %%c --rl_method dqn --net %%n --start_date 20220806090000 --end_date 20221106000000
 
-python main.py --mode update --ver v3 --name 005930 --stock_code 005930 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode update --ver v3 --name 005380 --stock_code 005380 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode update --ver v3 --name 035720 --stock_code 035720 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
+        @REM predict
+        python main.py --mode predict --ver v1 --name %%c --stock_code %%c --rl_method dqn --net %%n --start_date 2022110609000 --end_date 20221207153500
 
-python main.py --mode predict --ver v3 --name 005930 --stock_code 005930 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode predict --ver v3 --name 005380 --stock_code 005380 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode predict --ver v3 --name 035720 --stock_code 035720 --rl_method a2c --net lstm --start_date 20200101 --end_date 20211231
+        @REM update
+        python main.py --mode update --ver v1 --name %%c --stock_code %%c --rl_method dqn --net %%n --start_date 20220806090000 --end_date 20221106000000
+    )
+)
 
-python main.py --mode train --ver v3 --name all --stock_code 005930 005380 035720 --rl_method a3c --net lstm --start_date 20180101 --end_date 20191231
-python main.py --mode test --ver v3 --name all --stock_code 005930 005380 035720 --rl_method a3c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode update --ver v3 --name all --stock_code 005930 005380 035720 --rl_method a3c --net lstm --start_date 20200101 --end_date 20211231
-python main.py --mode predict --ver v3 --name all --stock_code 005930 005380 035720 --rl_method a3c --net lstm --start_date 20200101 --end_date 20211231
+for %%n in (dnn lstm cnn) do (
+    @REM train
+    python main.py --mode train --ver v1 --name all --stock_code 005930 035720 005490 --rl_method pg --net %%n --start_date 20211206090000 --end_date 20220806000000
 
-python main.py --mode test --ver v3 --name 005930a --stock_code 005930 --rl_method monkey --net monkey --start_date 20180101 --end_date 20191231
-python main.py --mode test --ver v3 --name 005930b --stock_code 005930 --rl_method monkey --net monkey --start_date 20200101 --end_date 20211231
-python main.py --mode test --ver v3 --name 005380a --stock_code 005380 --rl_method monkey --net monkey --start_date 20180101 --end_date 20191231
-python main.py --mode test --ver v3 --name 005380b --stock_code 005380 --rl_method monkey --net monkey --start_date 20200101 --end_date 20211231
-python main.py --mode test --ver v3 --name 035720a --stock_code 035720 --rl_method monkey --net monkey --start_date 20180101 --end_date 20191231
-python main.py --mode test --ver v3 --name 035720b --stock_code 035720 --rl_method monkey --net monkey --start_date 20200101 --end_date 20211231
+    @REM test
+    python main.py --mode test --ver v1 --name all --stock_code 005930 035720 005490 --rl_method pg --net %%n --start_date 20220806090000 --end_date 20221106000000
+
+    @REM predict
+    python main.py --mode predict --ver v1 --name all --stock_code 005930 035720 005490 --rl_method pg --net %%n --start_date 2022110609000 --end_date 20221207153500
+
+    @REM update
+    python main.py --mode update --ver v1 --name all --stock_code 005930 035720 005490 --rl_method pg --net %%n --start_date 20220806090000 --end_date 20221106000000
+)
